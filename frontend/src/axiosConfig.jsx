a4f8ebb9eb50d3eca8428a85a8ec3e77ae6fc2b4
Khushboo Bissa <khushboo.bissa@gmail.com>
2025-08-11 15:54:48 +1000

    changed in testing and workflows

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  // baseURL: 'http://localhost:5001/api', // local
+  baseURL: 'http://3.25.109.164:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
