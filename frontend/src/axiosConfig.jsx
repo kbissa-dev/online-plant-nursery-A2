b76b66acc92444fc4fbc17d543ca6c3ea8c675f2
ChiChi <y3.yip@connect.qut.edu.au>
2025-09-21 04:29:57 +1000

    updated axiosConfig

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001',
+  // baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001',
+  baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 export default axiosInstance;
