29b1e4f1b6b79f09f2d24588b14422267ac74f50
ChiChi <y3.yip@connect.qut.edu.au>
2025-09-21 15:10:34 +1000

    update PlantsPage to handle different users,

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -6,4 +6,29 @@ const axiosInstance = axios.create({
   baseURL: '/api',
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('authToken');
+    }
+    return Promise.reject(error);
+  }
+);
+
+
 export default axiosInstance;
