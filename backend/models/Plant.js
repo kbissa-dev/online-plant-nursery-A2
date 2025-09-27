e46811817639ec92f8a6302e957e4e3ab9e2c124
ChiChi <y3.yip@connect.qut.edu.au>
2025-09-27 23:27:26 +1000

    feat/plantImage

--- backend/models/Plant.js
+++ backend/models/Plant.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const PlantSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
+  image: { type: String, default: 'placeholder.jpg' },
   price: { type: Number, required: true, min: 0 },
   stock: { type: Number, default: 0, min: 0 },
   description: { type: String },
   category: { type: String },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // optional
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Plant', PlantSchema);
