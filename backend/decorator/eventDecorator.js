55e32a180f8f6c003d20bc60fdd3561095076dfa
jadeliu910 <jingya.liu@connect.qut.edu.au>
2025-09-30 17:15:02 +1000

    Update eventDecorator.js

--- backend/decorator/eventDecorator.js
+++ backend/decorator/eventDecorator.js
@@ -1,18 +1,47 @@
-// backend/decorator/eventDecorator.js
+// Base Decorator
 class EventDecorator {
   constructor(event) {
     this.event = event;
   }
 
-  formatForCommunityFeed() {
+  format() {
     return {
       id: this.event._id,
       title: this.event.title ? this.event.title.toUpperCase() : '',
       date: this.event.date ? new Date(this.event.date).toLocaleDateString() : null,
       location: this.event.location || '',
-      isFeatured: Array.isArray(this.event.attendees) && this.event.attendees.length > 100, // highlight popular events
+      isFeatured:
+        Array.isArray(this.event.attendees) && this.event.attendees.length > 100,
     };
   }
 }
 
-module.exports = EventDecorator;
+// City-based Decorators
+class SydneyEventDecorator extends EventDecorator {
+  format() {
+    const base = super.format();
+    return { ...base, cityTag: "Sydney" };
+  }
+}
+
+class MelbourneEventDecorator extends EventDecorator {
+  format() {
+    const base = super.format();
+    return { ...base, cityTag: "Melbourne" };
+  }
+}
+
+class BrisbaneEventDecorator extends EventDecorator {
+  format() {
+    const base = super.format();
+    return { ...base, cityTag: "Brisbane" };
+  }
+}
+
+// Export all decorators
+module.exports = {
+  EventDecorator,
+  SydneyEventDecorator,
+  MelbourneEventDecorator,
+  BrisbaneEventDecorator
+};
